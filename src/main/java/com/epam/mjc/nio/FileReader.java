f3d396ae89e041de8475f45bfc086930d06cdab7
Sergey <spapoyan1987@gmail.com>
2022-12-21 15:57:02 +0400

    commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder temp = new StringBuilder();
+        try (RandomAccessFile aFile = new RandomAccessFile("src\\main\\resources\\Profile.txt", "r")) {
+            FileChannel channel = aFile.getChannel();
+            long size = channel.size();
+            ByteBuffer buffer = ByteBuffer.allocate((int) size);
+            channel.read(buffer);
+            buffer.flip();
+            for (int i = 0; i < size; i++) {
+                temp.append((char) buffer.get());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] split = temp.toString().split("\r\n");
+        String[] values = new String[4];
+        for (int i = 0; i < split.length; i++) {
+            String[] split1 = split[i].split(": ");
+            values[i] = split1[1];
+        }
+        return new Profile(values[0], Integer.parseInt(values[1]), values[2], Long.parseLong(values[3]));
     }
 }
